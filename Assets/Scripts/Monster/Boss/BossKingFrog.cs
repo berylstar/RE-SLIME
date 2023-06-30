27955aa8d3063a5de73761c39712b2dc80a7955f
berylstar <minsang0610@naver.com>
2023-06-30 22:07:34 +0900

    0630

---
+++ Assets/Scripts/Monster/Boss/BossKingFrog.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossKingFrog : MonsterScript
+{
+    [Header("SPAWNER")]
+    public GameObject frog;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        StartCoroutine(MonsterMove());
+        StartCoroutine(SpawnCo());
+    }
+
+    private void OnDestroy()
+    {
+        BoardManager.I.DropBox(transform.position);
+    }
+
+    IEnumerator SpawnCo()
+    {
+        while (isAlive)
+        {
+            yield return GameController.delay_3s;
+            Spawn();
+            Spawn();
+        }
+    }
+
+    private void Spawn()
+    {
+        GameObject product = Instantiate(frog, BoardManager.I.SpawnPosition(), Quaternion.identity) as GameObject;
+        product.transform.SetParent(GameObject.Find("ObjectHolder").transform);
+    }
+}
